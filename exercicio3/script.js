216f2c034fc69c54039a366c1c75999e2306bbec
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-08-30 16:27:22 -0300

    primeiro commit

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const operaUm = 5 > 20 && 5 < 2
+console.log(operaUm)
+
+const operaDois = 5 === 5 || 5 === "5"
+console.log(operaDois)
+
+const operaTrês = !(20 > 50)
+console.log(operaTrês)
+
+const operaQuatro = !(20 > 50 || 50 > 60)
+console.log(operaQuatro)
