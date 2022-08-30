47a19575e34c400000af07a694881579c29247bd
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-08-30 15:43:01 -0300

    primeiro commit

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+
+let usuario = Number(prompt("Insira um número par"))
+console.log(usuario % 2)
+
+// O padrão ao colocar diversos numeros pares é que o resto sempre será zero
+// Ao inserir um número impar o resto sempre será um
+
+
+
+
